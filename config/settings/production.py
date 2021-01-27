ea4999ff970101219168eb28053ccf6704d157bd
gabe <ggetzie@gmail.com>
2021-01-27 11:28:35 +0800

    test

--- config/settings/production.py
+++ config/settings/production.py
@@ -8,7 +8,8 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", 
                          default=["expensivetaste.art",
-                                  "www.expensivetaste.art"])
+                                  "www.expensivetaste.art",
+                                  "localhost"])
 
 # DATABASES
 # ------------------------------------------------------------------------------
