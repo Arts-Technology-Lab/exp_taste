45dcb08a7792ba82b8c4dac90f84d198792a8973
gabe <ggetzie@gmail.com>
2021-05-13 07:08:35 +0800

    select random by index rather than choice from qs

--- main/views.py
+++ main/views.py
@@ -11,18 +11,22 @@ class Home(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        a = random.choice(AuctionLot
-                          .objects
-                          .filter(collected=True, sale_price__isnull=False))
+        qs1 = AuctionLot.objects.filter(collected=True, 
+                                        sale_price__isnull=False)
+        idx1 = random.randint(0, qs1.count() - 1)
+        a = qs1[idx1]
+        
                           
         min_year = a.auction.end_date.year - 1
         max_year = a.auction.end_date.year + 1
-        b = random.choice(AuctionLot
-                          .objects
-                          .filter(collected=True, sale_price__isnull=False,
-                                 auction__end_date__year__gte=min_year,
-                                 auction__end_date__year__lte=max_year)
-                          .exclude(id=a.id))
+        qs2 = (AuctionLot
+              .objects
+              .filter(collected=True, sale_price__isnull=False,
+                      auction__end_date__year__gte=min_year,
+                      auction__end_date__year__lte=max_year)
+              .exclude(id=a.id))
+        idx2 = random.randint(0, qs2.count()-1)
+        b = qs2[idx2]
         context["lot_a"] = a
         context["lot_a_img"] = a.lotimage_set.all()[0]
         context["lot_b"] = b
