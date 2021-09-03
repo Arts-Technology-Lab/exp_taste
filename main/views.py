f0d8f156e3b534f67a8e08b8d63a78eece5e09c8
Gabriel Getzie <ggetzie@hku.hk>
2021-09-03 17:09:50 +0800

    about page

--- main/views.py
+++ main/views.py
@@ -13,23 +13,24 @@ class Home(TemplateView):
         context = super().get_context_data(**kwargs)
         qs1 = AuctionLot.objects.filter(collected=True, 
                                         sale_price__isnull=False)
-        idx1 = random.randint(0, qs1.count() - 1)
-        a = qs1[idx1]
-        
-                          
-        min_year = a.auction.end_date.year - 1
-        max_year = a.auction.end_date.year + 1
-        qs2 = (AuctionLot
-              .objects
-              .filter(collected=True, sale_price__isnull=False,
-                      auction__end_date__year__gte=min_year,
-                      auction__end_date__year__lte=max_year)
-              .exclude(id=a.id))
-        idx2 = random.randint(0, qs2.count()-1)
-        b = qs2[idx2]
-        context["lot_a"] = a
-        context["lot_a_img"] = a.lotimage_set.all()[0]
-        context["lot_b"] = b
-        context["lot_b_img"] = b.lotimage_set.all()[0]
+        if qs1.count() > 0:                                        
+            idx1 = random.randint(0, qs1.count() - 1)
+            a = qs1[idx1]
+            
+                            
+            min_year = a.auction.end_date.year - 1
+            max_year = a.auction.end_date.year + 1
+            qs2 = (AuctionLot
+                .objects
+                .filter(collected=True, sale_price__isnull=False,
+                        auction__end_date__year__gte=min_year,
+                        auction__end_date__year__lte=max_year)
+                .exclude(id=a.id))
+            idx2 = random.randint(0, qs2.count()-1)
+            b = qs2[idx2]
+            context["lot_a"] = a
+            context["lot_a_img"] = a.lotimage_set.all()[0]
+            context["lot_b"] = b
+            context["lot_b_img"] = b.lotimage_set.all()[0]
 
         return context
